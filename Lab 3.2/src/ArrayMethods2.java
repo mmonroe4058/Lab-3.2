521ae769e1966b483ba9a03676c6ec53a462f3dd
BT_1N3_02 <BT_1N3_02@BT_1N3_02.bktech.local>
2017-11-16 13:17:02 -0500

    arraymethods2

---
+++ Lab 3.2/src/ArrayMethods2.java
@@ -0,0 +1,23 @@
+
+public class ArrayMethods2
+{
+	public static void main (String[] args)
+	{
+		
+	}
+	public static String[] merge(String[] list1, String[] list2)
+	{
+		String[] merged = new String[list1.length + list2.length];
+		for(int i = 0; i < list1.length; i++)
+		{
+			for(int j = 0; j < list2.length; j++)
+			{
+				if(list1[i].compareTo(list2[j]) < 0)
+				{
+					
+				}
+			}
+		}
+	}
+
+}
