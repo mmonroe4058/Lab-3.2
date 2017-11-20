ca9a019f3b7d25cf5fcf432f91eddf1c059485d1
BT_1N3_03 <BT_1N3_03@BT_1N3_03.bktech.local>
2017-11-20 13:15:10 -0500

    merge

--- Lab 3.2/src/ArrayMethods2.java
+++ Lab 3.2/src/ArrayMethods2.java
@@ -1,22 +1,73 @@
-
+/*
+ * Miriam Monroe and Ramisha Sarwar
+ * Lab 3.2
+ */
 public class ArrayMethods2
 {
 	public static void main (String[] args)
 	{
+		/*String [] stuff1 = {"orange","banana", "paper"};
+		String [] stuff2 = {"apple", "rock"};*/
+		String [] stuff1 = {"A", "C", "E", "G"};
+		String [] stuff2 = {"B", "D", "F", "H"};
 		
+		printArr(merge(stuff1, stuff2));
 	}
 	public static String[] merge(String[] list1, String[] list2)
 	{
 		String[] merged = new String[list1.length + list2.length];
-		for(int i = 0; i < list1.length; i++)
+		int index = 0;
+		for(int i = 0; i < list1.length && i<list2.length; i++)
 		{
-			for(int j = 0; j < list2.length; j++)
-			{
-				if(list1[i].compareTo(list2[j]) < 0)
+				merged[index] = list1[i];
+				merged[index+1]= list2[i];
+				index += 2;
+		}
+				if(list1.length > list2.length)
 				{
-					
+					for(int i = list2.length; i <list1.length; i++)
+					{
+						merged[index] = list1[i];
+					}
 				}
-			}
+				else 
+				{
+					for(int i = list1.length; i < list2.length; i++)
+					{
+						merged[index] = list2[i];
+					}
+				}
+		return merged;
+	}
+	
+	public static String[] mergeSort(String[] list)
+	{
+		/*if(list1[i].compareTo(list2[j]) < 0)
+		{
+			String temp = list2[j];
+			list2[j] = list1[i];
+			list1[i] = temp;
+		}
+		else if(list1[i].compareTo(list2[j])> 0)
+		{
+			
+		}*/
+		int index = list.length/2;
+		if(list.length <= 1)
+		{
+			return list;
+		}
+		else 
+		{
+			return merge(mergeSort(list), mergeSort())
+					//copyOf
+		}
+	}
+	public static void printArr(String[] arr)
+	{
+		for(int i = 0; i<arr.length; i++)
+		{
+			System.out.print(arr[i]);
 		}
 	}
  
